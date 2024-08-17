da1aa2e81014705a1ffb0c6599134342fd5426c3
treaditup <roman.pozharnov@yandex.ru>
2024-08-18 00:49:43 +0300

    Add span decorator example in real world

--- examples/real_world/app/routes/slow.py
+++ examples/real_world/app/routes/slow.py
@@ -1,8 +1,11 @@
 import asyncio
 import logging
+import random
 
 from fastapi import APIRouter, status
 from opentelemetry import trace
+
+from asgi_monitor.tracing import span
 
 slow_router = APIRouter(
     prefix="/slow",
@@ -28,3 +31,19 @@ async def get_1000ms() -> dict:
         await asyncio.sleep(0.4)
         logger.info("full energy")
     return {"message": "ok", "status": "success"}
+
+
+@span
+def nested_func() -> int:
+    num = random.randint(1, 10)  # noqa: S311
+    current_span = trace.get_current_span()
+    current_span.set_attribute("num", num)
+    current_span.add_event("num rendered")
+    return num
+
+
+@slow_router.get("/span", status_code=status.HTTP_200_OK)
+@span(name="span handler", attributes={"foo": "bar"})
+async def get_span() -> dict:
+    num = nested_func()
+    return {"message": "ok", "status": "success", "num": num}
